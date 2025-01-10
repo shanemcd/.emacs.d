e5e2729fb2e8398ff35caf6797818e6f4ecee668
Shane McDonald <smcdonal@redhat.com>
2025-01-10 11:04:18 -0500

    Fix bug where we were being prompted every time to trust themes when using auto-dark-mode

--- init.el
+++ init.el
@@ -2,5 +2,13 @@
 ;;; Commentary:
 ;;; Not sure what more to say here.  Have a nice day.
 ;;; Code:
+
+;; This is hopefully temporary. The code inserted into generated.el needs to be
+;; run before the function call below, specifically so that the value for
+;; custom-safe-themes is respected.
+(setq custom-file (expand-file-name "generated.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 (org-babel-load-file "~/.emacs.d/readme.org")
 ;;; init.el ends here
